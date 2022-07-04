a5be89bba459b637e541528c3604410fce847fe1
Leo <leo.maedje@gmail.com>
2022-07-04 12:44:07 +0100

    Changed variable names

--- src/count-occurrences.js
+++ src/count-occurrences.js
@@ -5,21 +5,21 @@
  * Partial matches are not counted.
  * The count should be case sensitive.
  *
- * @param {string[]} y - array of strings to check
- * @param {string} n - string to count occurrences off
+ * @param {string[]} inputArr - array of strings to check
+ * @param {string} searchStr - string to count occurrences off
  * @returns {number} how many occurrences of the string are in the array
  */
-function x(y, n) {
-  let x2 = 0;
-  for (let mars of y) {
-    if ((n === mars)) {
-      x2++;
+function countOccurrences(inputArr, searchStr) {
+  let count = 0;
+  for (let arrayStr of inputArr) {
+    if ((arrayStr === searchStr)) {
+      count++;
     }
   }
-  return x2;
+  return count;
 }
 
-console.log(x(["na", "na", "na", "na", "BATMAN"], "na"), 4);
-console.log(x(["na", "na", "na", "na", "BATMAN"], "BATMAN"), 1);
-console.log(x(["na", "na", "na", "na", "BATMAN"], "batman"), 0);
-console.log(x(["na", "na", "na", "na", "BATMAN"], "a"), 0);
+console.log(countOccurrences(["na", "na", "na", "na", "BATMAN"], "na"), 4);
+console.log(countOccurrences(["na", "na", "na", "na", "BATMAN"], "BATMAN"), 1);
+console.log(countOccurrences(["na", "na", "na", "na", "BATMAN"], "batman"), 0);
+console.log(countOccurrences(["na", "na", "na", "na", "BATMAN"], "a"), 0);
